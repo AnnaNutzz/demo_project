81ced3ddbb6344606a343b51adfb3d521378fe08
AnnaNutzz <85386175+AnnaNutzz@users.noreply.github.com>
2025-03-28 09:56:52 +0530

    Update addition.java for textual data

--- calculator/addition.java
+++ calculator/addition.java
@@ -1,5 +1,11 @@
 public class Addition {
+    // Method for adding two numbers
     public int add(int a, int b) {
         return a + b;
+    }
+
+    // Method for concatenating two strings
+    public String add(String str1, String str2) {
+        return str1 + str2;
     }
 }
