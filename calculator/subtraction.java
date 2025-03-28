8d1d9a8358f05f62bdccfec6f46e49eeb8924590
AnnaNutzz <85386175+AnnaNutzz@users.noreply.github.com>
2025-03-28 09:57:24 +0530

    Update subtraction.java for textual data

--- calculator/subtraction.java
+++ calculator/subtraction.java
@@ -1,5 +1,11 @@
 public class Subtraction {
+    // Method for subtracting two numbers
     public int subtract(int a, int b) {
         return a - b;
+    }
+
+    // Method for removing the second string occurrence from the first
+    public String subtract(String mainStr, String subStr) {
+        return mainStr.replace(subStr, "");  // Removes all occurrences
     }
 }
